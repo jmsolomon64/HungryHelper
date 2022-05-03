21e2e993152afba1c2553fbc2f8600c5d99cfe71
cstazinski29 <cstazinski@gmail.com>
2022-05-02 21:10:36 -0400

    added InitialUserProfileCreate migration

---
+++ HungryHelper.Data/Migrations/20220503010745_InitialUserProfileCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HungryHelper.Data.Migrations
+{
+    public partial class InitialUserProfileCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserProfile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateJoined = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CookingExperienceLevel = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FavoriteFood = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfile", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserProfile");
+        }
+    }
+}
