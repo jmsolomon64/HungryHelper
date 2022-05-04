6cbb594f1f153220b8e0700a86c7a94d0125a3b4
Scott <sfitzpatrick55@gmail.com>
2022-05-03 20:58:58 -0400

    Add Amount to ShoppingList entity

--- HungryHelper.Services/FavoritedRecipes/FavoritedRecipesService.cs
+++ HungryHelper.Services/FavoritedRecipes/FavoritedRecipesService.cs
@@ -1,8 +1,17 @@
+using HungryHelper.Models.FavoritedRecipes;
+using HungryHelper.Data.Entities;
 
 namespace HungryHelper.Services.FavoritedRecipes
 {
     public class FavoritedRecipesService : IFavoritedRecipesService
     {
-        
+        public async Task<bool> CreateFavoritedRecipesAsync(FavoritedRecipesCreate model);
+
+        var entity = new FavoritedRecipesEntity
+        {
+            Id = Models.Id,
+            UserId = model.
+            RecipeId
+        }
     }
 }
