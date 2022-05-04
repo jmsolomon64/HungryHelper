27a87f56a6f96f2ad478d6f0e39bc980080668e3
Jacob <jmsolomon64@gmail.com>
2022-05-04 02:31:40 -0400

    COntroller has post method

--- HungryHelper.WebAPI/Controllers/IngredientController.cs
+++ HungryHelper.WebAPI/Controllers/IngredientController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HungryHelper.Models.Ingredient;
 using HungryHelper.Services.Ingredient;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +17,25 @@ namespace HungryHelper.WebAPI.Controllers
         public IngredientController(IIngredientServices service) //dependency injection
         {
             _service = service;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterIngredient ([FromBody] IngredientRegister model)
+        {
+            {
+                if(!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var registResult = await _service.RegisterIngredientAsync(model);
+                if (registResult)
+                {
+                    return Ok("Ingredient was registere");
+                }
+
+                return BadRequest("Ingredient couldn't be registered");
+            }
         }
     }
 }
