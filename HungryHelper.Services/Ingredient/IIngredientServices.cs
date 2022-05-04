d225c0ec82a52a2ded1f4bdac12db41a75053564
Jacob <jmsolomon64@gmail.com>
2022-05-04 02:25:58 -0400

    Ingredients service added

--- HungryHelper.Services/Ingredient/IIngredientServices.cs
+++ HungryHelper.Services/Ingredient/IIngredientServices.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HungryHelper.Models.Ingredient;
 
 namespace HungryHelper.Services.Ingredient
 {
     public interface IIngredientServices
     {
-        
+        Task<bool> RegisterIngredientAsync(IngredientRegister model);
     }
 }
--- HungryHelper.Services/Ingredient/IngredientServices.cs
+++ HungryHelper.Services/Ingredient/IngredientServices.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HungryHelper.Data;
+using HungryHelper.Data.Entities;
+using HungryHelper.Models.Ingredient;
 
 namespace HungryHelper.Services.Ingredient
 {
     public class IngredientService : IIngredientServices
     {
-        
+        private readonly ApplicationDbContext _context;
+        public IngredientService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> RegisterIngredientAsync(IngredientRegister model)
+        {
+            var entity = new IngredientEntity
+            {
+                Name = model.Name
+            };
+            _context.Ingredients.Add(entity);
+            
+        }
     }
 }
