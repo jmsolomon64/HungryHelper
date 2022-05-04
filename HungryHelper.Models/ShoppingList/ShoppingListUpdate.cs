970d3884509ad76f1fcec78403e22f1dee7db7ba
Scott <sfitzpatrick55@gmail.com>
2022-05-03 21:11:22 -0400

    Add ListId to ShoppingListUpdate

--- HungryHelper.Models/ShoppingList/ShoppingListUpdate.cs
+++ HungryHelper.Models/ShoppingList/ShoppingListUpdate.cs
@@ -6,6 +6,9 @@ namespace HungryHelper.Models.ShoppingList
     public class ShoppingListUpdate
     {
         [Required]
+        public int ListId { get; set; }
+        
+        [Required]
         public string IngredientName { get; set; }
 
         public string Amount { get; set; }
