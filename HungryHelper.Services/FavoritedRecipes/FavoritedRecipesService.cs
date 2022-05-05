3aad7c588ddb2b93f94be79d268df2ad677f478d
Scott <sfitzpatrick55@gmail.com>
2022-05-04 20:21:17 -0400

    Build favoritedrecipe async

--- HungryHelper.Services/FavoritedRecipes/FavoritedRecipesService.cs
+++ HungryHelper.Services/FavoritedRecipes/FavoritedRecipesService.cs
@@ -2,18 +2,29 @@ using System;
 using System.Threading.Tasks;
 using HungryHelper.Models.FavoritedRecipes;
 using HungryHelper.Data.Entities;
+using HungryHelper.Data;
 
 namespace HungryHelper.Services.FavoritedRecipes
 {
     public class FavoritedRecipesService : IFavoritedRecipesService
     {
-        public async Task<bool> CreateFavoritedRecipesAsync(FavoritedRecipesCreate model);
-
-        var entity = new FavoritedRecipesEntity
+        private readonly ApplicationDbContext _context;
+        public FavoritedRecipesService(ApplicationDbContext context)
         {
-            Id = model.Id,
-            UserId = model.UserId,
-            RecipeId = model.RecipeId
-        };
+            _context = context;
+        }
+        public async Task<bool> CreateFavoritedRecipesAsync(FavoritedRecipesCreate model)
+        {
+            var entity = new FavoritedRecipesEntity
+            {
+                UserId = model.UserId,
+                RecipeId = model.RecipeId
+            };
+
+            _context.FavoritedRecipes.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
     }
 }
