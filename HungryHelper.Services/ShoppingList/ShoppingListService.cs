704ac496333b678448b8641c335a2852011fa4f8
Scott <sfitzpatrick55@gmail.com>
2022-05-04 20:21:40 -0400

    Remove unused using statements

--- HungryHelper.Services/ShoppingList/ShoppingListService.cs
+++ HungryHelper.Services/ShoppingList/ShoppingListService.cs
@@ -1,12 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using HungryHelper.Data;
+using HungryHelper.Data.Entities;
+using HungryHelper.Models.ShoppingList;
 
 namespace HungryHelper.Services.ShoppingList
 {
     public class ShoppingListService : IShoppingListService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public ShoppingListService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> CreateShoppingListAsync(ShoppingListCreate model)
+        {
+            var entity = new ShoppingListEntity
+            {
+                IngredientName = model.IngredientName,
+                Amount = model.IngredientName,
+                DateAdded = DateTime.Now
+            };
+
+            _context.ShoppingList.Add(entity);
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            return numberOfChanges == 1;
+        }
     }
 }
