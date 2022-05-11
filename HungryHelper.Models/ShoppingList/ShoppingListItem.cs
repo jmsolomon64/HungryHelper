ad8df3ca175c37fff8cac864b8f8f7939f31e427
Scott <sfitzpatrick55@gmail.com>
2022-05-11 01:36:43 -0400

    Update get set for ShoppingListIem

--- HungryHelper.Models/ShoppingList/ShoppingListItem.cs
+++ HungryHelper.Models/ShoppingList/ShoppingListItem.cs
@@ -7,6 +7,9 @@ namespace HungryHelper.Models.ShoppingList
 {
     public class ShoppingListItem
     {
-        
+        public int Id { get; set; }
+        public string IngredientName { get; set; }
+        public string Amount { get; set; }
+        public DateTimeOffset UtcCreated { get; set; }
     }
 }
