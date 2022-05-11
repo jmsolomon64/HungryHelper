73931715788e16e88a255bf94b2b3bd539881db8
Scott <sfitzpatrick55@gmail.com>
2022-05-10 23:21:12 -0400

    UserProfile and shopping list updated migration and FK

---
+++ HungryHelper.Data/Migrations/20220511031525_UpdatedShoppingListandFKAdded.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HungryHelper.Data.Migrations
+{
+    public partial class UpdatedShoppingListandFKAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
