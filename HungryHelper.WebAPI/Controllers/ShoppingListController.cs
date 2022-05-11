b547b2cc3043b188142b619edf2c39cb9f6c65cd
Scott <sfitzpatrick55@gmail.com>
2022-05-10 23:24:59 -0400

    Add Post and Get to Shopping List controller

--- HungryHelper.WebAPI/Controllers/ShoppingListController.cs
+++ HungryHelper.WebAPI/Controllers/ShoppingListController.cs
@@ -1,5 +1,6 @@
 using HungryHelper.Services.ShoppingList;
 using Microsoft.AspNetCore.Mvc;
+using HungryHelper.Models.ShoppingList;
 
 namespace HungryHelper.WebAPI.Controllers
 {
@@ -12,5 +13,49 @@ namespace HungryHelper.WebAPI.Controllers
         {
             _service = service;
         }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterShoppingList ([FromBody] ShoppingListCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var registerResult = await _service.CreateShoppingListAsync (model);
+            if (registerResult)
+            {
+                return Ok("A shopping list was create.");
+            }
+
+            return BadRequest("A shopping list could not be created.");
+        }
+
+        // [HttpGet("{userId:int}")]
+        // public async Task<IActionResult> GetShoppingListByUserId ([FromRoute] int userId)
+        // {
+        //     var shoppingListResult = await _service.GetShoppingListByUserIdAsync(userId);
+
+        //     if (shoppingListResult is null)
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     return Ok(shoppingListResult);
+        // }
+
+        // [HttpPut("{userId:int}")]
+        // public async Task<IActionResult> UpdateShoppingListById([FromForm] ShoppingListUpdate request)
+        // {
+        //     if (!ModelState.IsValid)
+        //     {
+        //         return BadRequest(ModelState);
+        //     }
+
+        //     return await _service.UpdateShoppingListByIdAsync(request)
+        //         ? Ok("Shopping list updated successfully.")
+        //         : BadRequest("Shopping list could not be updated.");
+        // }
+
     }
 }
