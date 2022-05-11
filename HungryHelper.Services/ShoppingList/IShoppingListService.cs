094dd3a23e0a3b9ae34d0c9c203c4d2d47bf2de8
Scott <sfitzpatrick55@gmail.com>
2022-05-11 18:54:58 -0400

    Add createShoppingListAsync method to shopping list interface

--- HungryHelper.Services/ShoppingList/IShoppingListService.cs
+++ HungryHelper.Services/ShoppingList/IShoppingListService.cs
@@ -5,6 +5,7 @@ namespace HungryHelper.Services.ShoppingList
 {
     public interface IShoppingListService
     {
+        Task<bool> CreateShoppingListAsync(ShoppingListCreate request);
         Task<IEnumerable<ShoppingListItem>> GetAllShoppingListByUserIdAsync();
     }
 }
