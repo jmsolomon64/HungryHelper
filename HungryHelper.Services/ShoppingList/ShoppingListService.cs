d64b13608d2d0f485492cd0857c7a1bc062aa8a9
Scott <sfitzpatrick55@gmail.com>
2022-05-10 23:59:17 -0400

    Build userId and user claims to validate inside shopping list service

--- HungryHelper.Services/ShoppingList/ShoppingListService.cs
+++ HungryHelper.Services/ShoppingList/ShoppingListService.cs
@@ -1,11 +1,22 @@
+using System.Security.Claims;
 using HungryHelper.Data;
 using HungryHelper.Data.Entities;
 using HungryHelper.Models.ShoppingList;
+using Microsoft.AspNetCore.Http;
 
 namespace HungryHelper.Services.ShoppingList
 {
     public class ShoppingListService : IShoppingListService
     {
+        private readonly int _userId; // establishing our _userId field
+        public ShoppingListService(IHttpContextAccessor httpContextAccessor)
+        {
+            var userClaims = httpContextAccessor.HttpContext.User.Identity as ClaimsIdentity;
+            var value = userClaims.FindFirst("Id")?.Value; // This is getting the user claims
+            var validId = int.TryParse(value, out _userId); // validate the claim value
+                if (!validId) // Handling an invalid Id
+                    throw new Exception("Attempted to build ShoppingListService without User Id claim.");
+        }
         private readonly ApplicationDbContext _context;
         public ShoppingListService(ApplicationDbContext context)
         {
