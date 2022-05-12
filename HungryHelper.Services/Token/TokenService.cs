310bad3cd56809f33600898d4cb33dcd26618d81
cstazinski29 <cstazinski@gmail.com>
2022-05-11 20:48:57 -0400

    worked in Token Service to add authorization; currently can get the token in postman, but cannot run the GET request

--- HungryHelper.Services/Token/TokenService.cs
+++ HungryHelper.Services/Token/TokenService.cs
@@ -15,9 +15,10 @@ namespace HungryHelper.Services.Token
     {
         private readonly ApplicationDbContext _context;
         private readonly IConfiguration _configuration;
-        public TokenService(ApplicationDbContext context)
+        public TokenService(ApplicationDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<TokenResponse> GetTokenAsync(TokenRequest model)
