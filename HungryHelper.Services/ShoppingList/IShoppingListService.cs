275da2b982c5e26df2bb10c5d77bd7d1d39909d4
Scott <sfitzpatrick55@gmail.com>
2022-05-12 20:26:32 -0400

    add line space

--- HungryHelper.Services/ShoppingList/IShoppingListService.cs
+++ HungryHelper.Services/ShoppingList/IShoppingListService.cs
@@ -6,6 +6,7 @@ namespace HungryHelper.Services.ShoppingList
     public interface IShoppingListService
     {
         Task<bool> CreateShoppingListAsync(ShoppingListCreate request);
+        
         Task<IEnumerable<ShoppingListItem>> GetAllShoppingListByUserIdAsync();
 
         Task<bool> UpdateShoppingListAsync(ShoppingListUpdate request);
