eb2e0b5470c8a7ba4260002658330e34425feef5
Jacob Solomon <jmsolomon64@gmail.com>
2022-05-12 20:21:08 -0400

    Fully functioning recipe controller

---
+++ HungryHelper.Data/Migrations/20220512224941_NewColumns.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HungryHelper.Data.Migrations
+{
+    public partial class NewColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RecipeEntityRecipeId",
+                table: "FavoritedRecipes",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavoritedRecipes_RecipeEntityRecipeId",
+                table: "FavoritedRecipes",
+                column: "RecipeEntityRecipeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavoritedRecipes_Recipes_RecipeEntityRecipeId",
+                table: "FavoritedRecipes",
+                column: "RecipeEntityRecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavoritedRecipes_Recipes_RecipeEntityRecipeId",
+                table: "FavoritedRecipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavoritedRecipes_RecipeEntityRecipeId",
+                table: "FavoritedRecipes");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeEntityRecipeId",
+                table: "FavoritedRecipes");
+        }
+    }
+}
