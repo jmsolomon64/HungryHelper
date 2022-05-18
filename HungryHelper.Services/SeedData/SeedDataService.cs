15c9b04f0330b7a7a8097bfa4bc0ef7c25503efa
cstazinski29 <cstazinski@gmail.com>
2022-05-17 21:25:03 -0400

    added (a possibly incorrect) outline for seed data

--- HungryHelper.Services/SeedData/SeedDataService.cs
+++ HungryHelper.Services/SeedData/SeedDataService.cs
@@ -1,7 +1,25 @@
+using HungryHelper.Data;
+
 namespace HungryHelper.Services.SeedData
 {
     public class SeedDataService : ISeedDataService
     {
-        
+        private readonly ApplicationDbContext _context;
+        public SeedDataService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        private void SeedUserProfiles()
+        {
+            // check whether UserProfile table has records in it
+            
+            // if there are records, exit method
+            // if no records (or less than 2 or something), add seed data
+        }
+        private void SeedIngredients() {}
+        private void SeedRecipes() {}
+        private void SeedShoppingList() {}
+        private void SeedFavoritedRecipes() {}
     }
 }
